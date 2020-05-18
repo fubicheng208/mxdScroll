e8ba7735cf97de54fcb215aec07f3f62c9a16145
fubicheng208 <qmhnaaa@gmail.com>
2020-05-19 00:02:18 +0800

    debug email test

--- src/main/java/com/fubic/mxd/scroll/mapper/ScrollMapper.java
+++ src/main/java/com/fubic/mxd/scroll/mapper/ScrollMapper.java
@@ -4,6 +4,13 @@ import com.fubic.mxd.scroll.model.WeaponScroll;
 
 import java.util.List;
 
+//        mybatis流程：
+//        1. 添加依赖
+//        2. 创建接口类
+//        3. 根据接口类的方法编写mapper.xml
+//        4. application.properties添加备注
+//        5. 启动类里添加mapper接口扫描
+
 public interface ScrollMapper {
     int save(WeaponScroll scroll);
 
